8f99428565697102a7a1aec1ea7c73e72b175602
Daniel <ddamek053@gmail.com>
2024-05-03 15:40:49 +0200

    Main structure in done

--- main.c
+++ main.c
@@ -1,96 +1,41 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
-  // char input;
-  // printf("For exit press 'ESC'\n");
-  // scanf("%s", &input);
-  while (1)
+  char str[] = "Hello World";
+  char guessedLetters[48] = {0};
+  char newStr[48] = {0};
+  int i = 0;
+
+  while (strcmp(str, newStr) != 0)
   {
-    int pocet = 0;
-    char letter;
-    printf("Enter a letter");
-    scanf("%c", &letter);
-    // input = getchar();
-    // if (input == 27)
-    // {
-    //   return 0;
-    // }
-    char str[] = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd'};
-    switch (letter)
+    printf("Enter a letter: ");
+    char letter = getchar();
+    getchar();
+
+    if (letter == '0')
     {
-    case 'A':
-    case 'a':
-    case 'B':
-    case 'b':
-    case 'C':
-    case 'c':
-    case 'D':
-    case 'd':
-    case 'E':
-    case 'e':
-    case 'F':
-    case 'f':
-    case 'G':
-    case 'g':
-    case 'H':
-    case 'h':
-    case 'I':
-    case 'i':
-    case 'J':
-    case 'j':
-    case 'K':
-    case 'k':
-    case 'L':
-    case 'l':
-    case 'M':
-    case 'm':
-    case 'N':
-    case 'n':
-    case 'O':
-    case 'o':
-    case 'P':
-    case 'p':
-    case 'Q':
-    case 'q':
-    case 'R':
-    case 'r':
-    case 'S':
-    case 's':
-    case 'T':
-    case 't':
-    case 'U':
-    case 'u':
-    case 'V':
-    case 'v':
-    case 'W':
-    case 'w':
-    case 'X':
-    case 'x':
-    case 'Y':
-    case 'y':
-    case 'Z':
-    case 'z':
-      for (int i = 0; i < str[i]; i++)
-      {
-        if (str[i] == letter)
-        {
-          printf("%c", str[i]);
-          pocet++;
-        }
-        else
-        {
-          printf("_");
-        }
-      }
-      break;
-
-    default:
-      break;
+      return 0;
     }
-    printf("%i", pocet+1);
 
-    
+    guessedLetters[i] = letter;
+    i++;
+
+    for (int j = 0; str[j] != '\0'; j++)
+    {
+      if (strchr(guessedLetters, str[j]) != NULL)
+      {
+        newStr[j] = str[j];
+      }
+      else
+      {
+        newStr[j] = '_';
+      }
+    }
+    printf("%s\n", newStr);
   }
+  
+  printf("You guesed the world!!");
   return 0;
 }
